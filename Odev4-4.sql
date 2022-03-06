daee9d46bc23c8882be566ad3ed7ff82b5acba8e
ahd1997 <76910474+ahd1997@users.noreply.github.com>
2022-03-06 16:01:01 +0300

    Create Odev4-4.sql

---
+++ Odev4-4.sql
@@ -0,0 +1,3 @@
+--dvdrental örnek veri tabanı üzerinden country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+SELECT COUNT(country) FROM country
+WHERE country LIKE '_____'
